601ddabe8830f03d4edbbaed387cbb0fd6755984
Trustday <dark_pride_shadow@hotmail.com>
2017-11-26 02:28:11 -0300

    Enemigo2
    Enemigo que se inmola

---
+++ Source/SquadShips2/CPP_Enemigo2Pawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CPP_Enemigo2Pawn.h"
+
+
+// Sets default values
+ACPP_Enemigo2Pawn::ACPP_Enemigo2Pawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_Enemigo2Pawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_Enemigo2Pawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACPP_Enemigo2Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/SquadShips2/CPP_Enemigo2Pawn.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CPP_Enemigo2Pawn.generated.h"
+
+UCLASS()
+class SQUADSHIPS2_API ACPP_Enemigo2Pawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACPP_Enemigo2Pawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	
+};
